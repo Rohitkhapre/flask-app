c7efdbb1cd512a9186fec66b8c3f4f513d01080c
root <root@ip-172-31-86-195.ec2.internal>
2023-06-20 12:43:04 +0000

    reduce the size of image

--- getsecret.py
+++
@@ -1,8 +0,0 @@
-import boto3
-
-client = boto3.client('secretsmanager')
-
-response = client.create_secret(
-    Name = 'MyDBsecret',
-    SecretString = '{"username": "admin", "password": "pass@123" }'
-)
